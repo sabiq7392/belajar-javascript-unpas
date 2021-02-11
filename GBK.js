05dd2a6f258421c6d209af77d019e73abcce55d4
Sabiq Muhammad <72848275+sabiq7392@users.noreply.github.com>
2021-02-11 22:13:48 +0700

    Add files via upload

--- GBK.js
+++ GBK.js
@@ -1,33 +1,40 @@
-var player = prompt('gunting, batu, kertas');
-var comp = Math.random();
-var hasil = '';
-
-
-
-if (comp < 0.34) {
-    comp = 'gunting';
-} else if (comp >= 0.34 && comp <= 0.67) {
-    comp = 'batu';
-} else {
-    comp = 'kertas';
+var tanya = true
+while(tanya) {
+    var player = prompt('gunting, batu, kertas');
+    var comp = Math.random();
+    var hasil = '';
+    
+    
+    
+    if (comp < 0.34) {
+        comp = 'gunting';
+    } else if (comp >= 0.34 && comp <= 0.67) {
+        comp = 'batu';
+    } else {
+        comp = 'kertas';
+    }
+    
+    if (player === comp) {
+        hasil += 'SERI!';
+    } else if (player == 'gunting') {
+        hasil += (comp == 'kertas') ? 'MENANG!' : 'KALAH'
+    } else if (player == 'batu') {
+        hasil += (comp == 'gunting') ? 'MENANG!' : 'KALAH'
+    } else if (player == 'kertas') {
+        hasil += (comp == 'batu') ? 'MENANG!' : 'KALAH'
+    } else {
+        hasil += 'SALAH INPUT';
+    }
+    // tampilan hasilnya
+    console.log(player);
+    console.log(comp);
+    console.log(hasil)
+    alert('KAMU memilih: ' + player + '\nKOMPUTER memilih: ' + comp + '\nhasilnya = KAMU ' + hasil)
+    
+    tanya = confirm('lagi?')
+    
 }
 
-if (player === comp) {
-    hasil += 'SERI!';
-} else if (player == 'gunting') {
-    hasil += (comp == 'kertas') ? 'MENANG!' : 'KALAH'
-} else if (player == 'batu') {
-    hasil += (comp == 'gunting') ? 'MENANG!' : 'KALAH'
-} else if (player == 'kertas') {
-    hasil += (comp == 'batu') ? 'MENANG!' : 'KALAH'
-} else {
-    hasil += 'SALAH INPUT';
-}
-// tampilan hasilnya
-console.log(player);
-console.log(comp);
-console.log(hasil)
-alert('KAMU memilih: ' + player + '\nKOMPUTER memilih: ' + comp + '\nhasilnya = KAMU ' + hasil)
-
+alert('terima kasih')
 
 
